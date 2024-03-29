63d18088dd0d8bfc804239a721c7f46b653caa7d
samreensami <samreensami>
2024-03-29 09:16:19 -0700

    Done

---
+++ index.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\twellcome to samcli-number-guessing game\n");
+
+const randomNumber = Math.floor(Math.random() *6 + 1);
+
+const answer = await inquirer.prompt(
+    [
+        {
+            name:"userGuessedNumber",
+            type:"number",
+            message:"enter your gussedNumber(number limit from 1 to 6):",
+        
+        },
+    ]
+);
+if(answer.userGuessedNumber===randomNumber)
+    {
+console.log("congratulation ! you Guessed a correct Number.");
+    }
+    else {console.log("sorry,you guessed a wrongNumber");
+
+    }
